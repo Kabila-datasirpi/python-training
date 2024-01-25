74c2cd8c811b81a0e61de92ffacd674ead25cf75
kabila-datasirpi <kabila.m@datasirpi.com>
2024-01-25 09:58:37 +0530

    modules branch added

---
+++ mod2.py
@@ -0,0 +1,13 @@
+import modu1
+# Using functions from the modu1 module
+result_sum = modu1.add(10, 5)
+print("Sum:", result_sum)
+
+result_difference = modu1.subtract(20, 7)
+print("Difference:", result_difference)
+
+result_product = modu1.multiply(8, 4)
+print("Product:", result_product)
+
+result_division = modu1.divide(50, 10)
+print("Division:", result_division)
