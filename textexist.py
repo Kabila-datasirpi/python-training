bb7f1825a25742b1ba1c26f1467d0619de4f7335
kabila-datasirpi <kabila.m@datasirpi.com>
2024-01-25 09:42:18 +0530

    textexist branch added

---
+++ textexist.py
@@ -0,0 +1,16 @@
+#string value exist test
+l=["ram","parvesh","jagan","gugan"]
+i="jack"
+if i in l:
+    print("text is in the list")
+else:
+    print("text is not in the list")
+
+#integer value exist in the list
+k=[1,2,6,8,10]
+j=7
+if j in k:
+    print("value is in the list")
+else:
+    print("value is not in the list")
+
