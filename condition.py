e4f55d7cee9f6b5beba2dfdad6286b5295d610ef
kabila-datasirpi <kabila.m@datasirpi.com>
2024-01-25 09:40:00 +0530

    conditions branch added

---
+++ condition.py
@@ -0,0 +1,60 @@
+a=10
+b=20
+#greater
+if a>b:
+    print("a is greater")
+else:
+    print("b is greater")
+
+#lesser
+if a<b:
+    print("a is lesser")
+else:
+    print("b is lesser")
+
+#equal
+if a==b:
+    print("a and b are equal")
+else:
+    print("not equal")
+
+#not equal
+if a!=b:
+    print("not equal")
+else:
+    print("a and b are equal")
+
+#greater than and equal
+if a>=b:
+    print("a is greater or equal to b")
+else:
+    print("a is not greater or equal to b")
+
+#less than and equal
+if a<=b:
+    print("a is lessthan or equal to b")
+else:
+    print("a is not lessthan or equal to b")
+
+#and
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+#or
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+#not
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+
+    
