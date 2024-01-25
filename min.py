4cfef91247b898d712d54fa8b9a0eccbfec241ee
kabila-datasirpi <kabila.m@datasirpi.com>
2024-01-25 09:37:00 +0530

    minmaxsum branch added

---
+++ min.py
@@ -0,0 +1,20 @@
+#minimum in numbers
+print(min(5,10))
+
+#minimum in strings it returns based on alphabet
+print(min("jack","aadhi","babu","kiran"))
+
+#sum()
+a=(10,20,30,40)
+print(sum(a))
+
+b=(2,3,9,7)
+print(sum(b,10))
+
+#range function
+for i in range(10):
+    print(i)
+
+#between 1 to 50
+for k in range(1,50):
+    print(k)
