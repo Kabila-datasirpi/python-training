cb73ce03199c8267ecb6012918439fa843bca69a
kabila-datasirpi <kabila.m@datasirpi.com>
2024-01-25 09:38:31 +0530

    boolean branch added

---
+++ bool.py
@@ -0,0 +1,18 @@
+#boolean return true or false only
+print(5>10)
+
+#this return true
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+#thease are return false
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+
