24624f53f3ac585d65268e27c699acb60fac67dd
kabila-datasirpi <kabila.m@datasirpi.com>
2024-02-07 12:30:59 +0530

    restadded

---
+++ restapp/serializers.py
@@ -0,0 +1,8 @@
+
+from rest_framework import serializers
+from .models import books
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = books
+        fields = ['id', 'title', 'description']
